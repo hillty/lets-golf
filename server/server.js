b50abb7743c6d4994d0665e320429329b35e8b8f
Ty Hill <hill.tyj22@gmail.com>
2022-03-01 16:09:20 -0700

    css connected to local host

--- server/server.js
+++ server/server.js
@@ -7,16 +7,19 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use('/landing.css', express.static(path.join(__dirname, '../public/landing.css')))
+app.use('/js', express.static(path.join(__dirname, '../public/index.js')))
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/landing.html'))
 })
-app.get('/styles', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.css'))
-})
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.js'))
-})
+// app.get('/styles', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/landing.css'))
+// })
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.js'))
+// })
 
 
 
