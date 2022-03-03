322de5980a1576aac937520eb6ce4b2d39a76be1
Ty Hill <hill.tyj22@gmail.com>
2022-03-02 21:40:24 -0700

    findCourseBtn functions to page

--- public/index.js
+++ public/index.js
@@ -1,1 +1,9 @@
 console.log('great success')
+
+// landing page button
+const findCourseBtn = document.querySelector('.findCourseBtn')
+// landing btn to find page
+findCourseBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.location.href="./find.html"
+})
