c034cc2e5c97788454879ca3b7bb89287efda73d
Ty Hill <hill.tyj22@gmail.com>
2022-03-02 18:36:39 -0700

    cleaned up names / comments

--- server/server.js
+++ server/server.js
@@ -7,13 +7,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-//landing page 
+
+// use
+    //landing
 app.use('/landing.css', express.static(path.join(__dirname, '../public/landing.css')))
 app.use('/js', express.static(path.join(__dirname, '../public/index.js')))
 app.use('/list.css', express.static(path.join(__dirname, '../public/list.css')))
 app.use('/find.css', express.static(path.join(__dirname, '../public/find.css')))
 
-
+// get
 app.get('/landing.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/landing.html'))
 })
