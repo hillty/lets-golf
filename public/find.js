c604874c09bed24532cfc225a8addc2bf03d540e
Ty Hill <hill.tyj22@gmail.com>
2022-03-08 08:55:28 -0700

    slider added / css details cleaned up

--- public/find.js
+++ public/find.js
@@ -5,21 +5,25 @@ const courseFinderInput = document.getElementById('courseFinderInput')
 const courseFinderTeeBtn = document.getElementById('courseFinderBtn')
 // zipCodeFinder back end call
 const zipCodeFinder = () => {
-    axios.get(`http://localhost:4207/${courseFinderInput.value}`)
-    .then(res => {
-
-        removeAllChildNodes(zipCodeContainer)
-        
-        const zipCodeCard = document.createElement('div')
-        zipCodeCard.classList.add('zipCodeCard')
-
-        zipCodeCard.innerHTML = `
-        <p id="courseName">Name- ${res.data.name}</p>
-        <p id="courseHoles">Holes- ${res.data.holes}</p>
-        `
-
-        zipCodeContainer.appendChild(zipCodeCard)
-    })
+    if(courseFinderInput.value !== "84062" && courseFinderInput.value !== "84097" && courseFinderInput.value !== "84004" && courseFinderInput.value !== "84604" && courseFinderInput.value !== "84003" && courseFinderInput.value !== "84606" && courseFinderInput.value !== "84043") {
+        alert('There is no course within this zip code.')
+    }else{
+        axios.get(`http://localhost:4207/${courseFinderInput.value}`)
+        .then(res => {
+            
+            removeAllChildNodes(zipCodeContainer)
+            
+            const zipCodeCard = document.createElement('div')
+            zipCodeCard.classList.add('zipCodeCard')
+            
+            zipCodeCard.innerHTML = `
+            <p id="courseName">Name- ${res.data.name}</p>
+            <p id="courseHoles">Holes- ${res.data.holes}</p>
+            `
+            
+            zipCodeContainer.appendChild(zipCodeCard)
+        })
+    }
 }
 
 const feelingLuckyBtn = document.getElementById('feelingLuckyBtn')
@@ -35,11 +39,12 @@ const randomCourseFinder = () => {
 
         feelingLuckyCard.innerHTML = `
         <p id="randomCourseName">Name- ${res.data.name}</p>
+        <p id="randomCourseZipCode">Zip Code- ${res.data.zipcode}</p>
         <p id="randomCourseHoles">Holes- ${res.data.holes}</p>
         `
 
         feelingLuckyContainer.appendChild(feelingLuckyCard)
-    })
+    }) 
 }
 
 
